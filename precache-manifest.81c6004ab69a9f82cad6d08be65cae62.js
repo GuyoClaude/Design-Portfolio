c50cc716d2ae8e63d46d08e497aa7ad592fba28a
claude009 <37622842+claude009@users.noreply.github.com>
2020-01-07 14:08:27 +0300

    Updates

---
+++ precache-manifest.81c6004ab69a9f82cad6d08be65cae62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33aacc9522a176d34ecb4a6882687db8",
+    "url": "/Graphics-Design-Portfolio/index.html"
+  },
+  {
+    "revision": "c0d2f610b3c4ae468f3e",
+    "url": "/Graphics-Design-Portfolio/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "cd1b2cb15787fd822936",
+    "url": "/Graphics-Design-Portfolio/static/js/2.2862792b.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Graphics-Design-Portfolio/static/js/2.2862792b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c0d2f610b3c4ae468f3e",
+    "url": "/Graphics-Design-Portfolio/static/js/main.04bfbe8c.chunk.js"
+  },
+  {
+    "revision": "f782e9a5a5b5ddf8eab9",
+    "url": "/Graphics-Design-Portfolio/static/js/runtime-main.513d293c.js"
+  }
+]);
